d17685cf5e4af03a2e40951a5e89d21d130e7ca3
ramonsantos <ramonsantos.pe@gmail.com>
2020-12-26 17:04:34 -0300

    Add credo and excoveralls

--- mix.exs
+++ mix.exs
@@ -7,6 +7,13 @@ defmodule ExMon.MixProject do
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ],
       deps: deps()
     ]
   end
@@ -21,8 +28,8 @@ defmodule ExMon.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:credo, "~> 1.5"},
+      {:excoveralls, "~> 0.10", only: :test}
     ]
   end
 end
