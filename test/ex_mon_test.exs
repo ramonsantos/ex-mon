d4bdf08143665a5a130cc00de7463a70a82fa405
ramonsantos <ramonsantos.pe@gmail.com>
2020-12-26 18:05:38 -0300

    Add tests

--- test/ex_mon_test.exs
+++ test/ex_mon_test.exs
@@ -1,5 +1,80 @@
 defmodule ExMonTest do
   use ExUnit.Case
 
-  # doctest ExMon
+  import ExUnit.CaptureIO
+
+  alias ExMon.{Game, Player}
+
+  describe "create_player/4" do
+    test "returns a player" do
+      expected_response = %Player{
+        life: 100,
+        moves: %{move_avg: :chute, move_heal: :cura, move_rnd: :soco},
+        name: "Ramon"
+      }
+
+      assert expected_response == ExMon.create_player("Ramon", :chute, :soco, :cura)
+    end
+  end
+
+  describe "start_game/1" do
+    test "when the game is started, returns a message" do
+      player = Player.build("Ramon", :chute, :soco, :cura)
+
+      messages =
+        capture_io(fn ->
+          assert ExMon.start_game(player) == :ok
+        end)
+
+      assert messages =~ "The game is started!"
+      assert messages =~ "status: :started"
+      assert messages =~ "turn: :player"
+    end
+  end
+
+  describe "make_move/1" do
+    setup do
+      player = Player.build("Ramon", :chute, :soco, :cura)
+
+      capture_io(fn ->
+        ExMon.start_game(player)
+      end)
+
+      {:ok, player: player}
+    end
+
+    test "when the move is valid, do the move and the computer makes a move" do
+      messages =
+        capture_io(fn ->
+          ExMon.make_move(:chute)
+        end)
+
+      assert messages =~ "The Player attacked the computer"
+      assert messages =~ "It's computer turn"
+      assert messages =~ "It's player turn"
+      assert messages =~ "status: :continue"
+    end
+
+    test "when the move is invalid, returns an error message" do
+      messages =
+        capture_io(fn ->
+          ExMon.make_move(:wrong)
+        end)
+
+      assert messages =~ "Invalid move: wrong."
+    end
+
+    test "when the move is valid, but game over, returns an error message", %{player: player} do
+      Game.info()
+      |> Map.put(:player, Map.put(player, :life, 0))
+      |> Game.update()
+
+      messages =
+        capture_io(fn ->
+          ExMon.make_move(:soco)
+        end)
+
+      assert messages =~ "The game is over."
+    end
+  end
 end
